9359a1a09f29edb9b3e249c22e3f4fabb9a30fbc
Zihao Yang <zyang25@stevens.edu>
2017-10-03 17:48:52 -0400

    Added missing number solution

---
+++ L268_Missing_Number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        newNums = [None] * (len(nums) + 1)
+
+        for x in range(0,len(nums)):
+        	newNums[nums[x]] = nums[x]
+
+
+
+        for ele in newNums:
+        	if ele == None:
+        		return newNums.index(ele)
+
+        
+
+nums = [0,1,3]
+print(Solution().missingNumber(nums))
