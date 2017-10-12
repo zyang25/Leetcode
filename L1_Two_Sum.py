01e4f7c75bf456e853c283f19cbbe99b7e9900c4
Zihao Yang <zyang25@stevens.edu>
2017-10-12 10:19:27 -0400

    Added two sum

---
+++ L1_Two_Sum.py
@@ -0,0 +1,32 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Example:
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        pointer = 0
+        twoSum = list()
+        indexY = 0
+        for x in nums[:]:
+        	for y in nums[indexY:]:
+        		if indexY + x == target:
+        			twoSum.add(nums.index(x))
+        			twoSum.add(nums.index(y))
+        			return twoSum
+        		indexY += 1
+
+Solution().twoSum([10,20,30,40], 70)
